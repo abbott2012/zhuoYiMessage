eb6f7887fc8295d8b00466911d99008ac629f9c5
雷峰峰 <11286637+lei-dahua@user.noreply.gitee.com>
2025-08-07 11:30:09 +0800

    first

---
+++ app/src/main/java/com/zhuoyi/message/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.zhuoyi.message
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.zhuoyi.message.ui.theme.MessageTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MessageTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MessageTheme {
+        Greeting("Android")
+    }
+}
---
+++ messagelibrary/src/main/java/com/zhuoyi/messagelibrary/MessageTest.java
@@ -0,0 +1,7 @@
+package com.zhuoyi.messagelibrary;
+
+public class MessageTest {
+    public int testNum(int number){
+        return  number * 2;
+    }
+}
